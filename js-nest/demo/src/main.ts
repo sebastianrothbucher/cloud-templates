88971d461d0b5d6ff42ba0645e005e989bc7dd6d
Sebastian Rothbucher <sebastianrothbucher@googlemail.com>
2023-11-10 22:02:13 +0100

    help debug - version info

--- js-nest/demo/src/main.ts
+++ js-nest/demo/src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import serverlessExpress from '@vendia/serverless-express'
 import { Callback, Context, Handler } from 'aws-lambda';
+import { APP_VERSION } from './version';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
+  console.log('Starting version ' + APP_VERSION + ' on port ' + port);
   const app = await NestFactory.create(AppModule);
   if (!process.env.LAMBDA_TASK_ROOT) {
-    await app.listen(3000);
+    await app.listen(port);
   } else {
     await app.init();
     return serverlessExpress({ app: app.getHttpAdapter().getInstance() });
---
+++ js-nest/demo/src/version.ts
@@ -0,0 +1,1 @@
+export const APP_VERSION = 'dev'; // automatically overwritten before build
